1351487ad3c62295325bb473df81eadab82e5ce4
carlos22-04 <car_22_04_95@hotmail.com>
2016-11-15 12:11:27 -0500

    Archivos subidos

---
+++ views/users/add.php
@@ -0,0 +1,27 @@
+<h2>Agregar Usuario</h2>
+
+<form action="<?php echo APP_URL."/users/add"; ?>" method="POST">
+    <p>
+        <label for="username">Username:</label>
+        <input type="text" name="username">
+    </p><br>
+    <p>
+        <label for="password">Password:</label>
+        <input type="password" name="password">
+    </p><br>
+    <p>
+        <label for="type_id">Types</label>
+        <select name="type_id" id="type_id">
+            <?php
+            foreach ($types as $type):?>
+            <option value="<?php echo $type["types"]["id"];?>">
+                <?php echo $type ["types"] ["name"];?>
+            </option>
+            <?php endforeach;?>
+        </select>
+    </p><br>
+    <p>
+        <input type="submit">
+    </p>
+
+</form>
---
+++ views/users/index.php
@@ -0,0 +1,42 @@
+<h2>Listado de usuarios</h2>
+<h4>Número de usuarios: </h4><?php echo $usersCount; ?>
+<?php if(!empty($users)): ?>
+<table>
+	<tr>
+		<th>ID</th>
+		<th>Username</th>
+		<th>Password</th>
+		<th>Type</th>
+		<th>Options</th>
+	</tr>
+	<?php
+		foreach ($users as $user): 
+	?>
+	<tr>
+		<td><?php echo $user["users"]["id"]; ?></td>
+		<td><?php echo $user["users"]["username"]; ?></td>
+		<td><?php echo $user["users"]["password"]; ?></td>
+		<td><?php echo $user["types"]["name"]; ?></td>
+		<td>
+            <?php
+            echo $this->Html->link("Edit", array(
+                "controller"=>"users",
+                "method"=>"edit",
+                "arg"=>$user["users"]["id"]
+));?> |
+            <?php
+           echo $this->Html->link("Delete", array(
+                "controller"=>"users",
+                "method"=>"delete",
+                "arg"=>$user["users"]["id"]
+            ));?>
+        </td>
+			<!--<a href="<?php echo APP_URL."/users/edit/".$user["users"]["id"]; ?>">Edit</a>-->
+			<!--<a href="<?php echo APP_URL."/users/delete/".$user["users"]["id"]; ?>">Delete</a>-->
+		</td>
+	</tr>
+	<?php 
+		endforeach; 
+	?>
+</table>
+<?php endif; ?>
